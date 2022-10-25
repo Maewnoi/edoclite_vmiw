5542daca96611d5015e53e4edd9272116f2fce6a
DomJi <sopita.arpaengpan@gmail.com>
2022-10-25 12:06:44 +0700

    update by domji

--- storage/framework/views/3f78132f710acfe07a60c84da83139e68b952799.php
+++ storage/framework/views/3f78132f710acfe07a60c84da83139e68b952799.php
@@ -1,10 +1,10 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<div class="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0 bg-gradient-to-b from-blue-200 to-blue-400">
     <div>
         <?php echo e($logo); ?>
 
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full px-6 py-4 mt-6 overflow-hidden shadow-md sm:max-w-md bg-blue-50 sm:rounded-lg">
         <?php echo e($slot); ?>
 
     </div>
