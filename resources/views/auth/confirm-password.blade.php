d155a0469850c65b49f8b1b0d24f25c686061b78
DomJi <sopita.arpaengpan@gmail.com>
2022-11-03 16:11:35 +0700

    update password.confirm bydomji

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,11 +1,12 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <img src="{{ asset('/image/logo_lei.png') }}" class="brand-image img-circle elevation-3" width="100"
+                height="100">
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+            {{ __('นี่คือพื้นที่ปลอดภัยของแอปพลิเคชัน โปรดยืนยันรหัสผ่านของคุณก่อนดำเนินการต่อครับ') }}
         </div>
 
         <x-jet-validation-errors class="mb-4" />
@@ -15,12 +16,12 @@
 
             <div>
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" autofocus />
+                <x-jet-input id="password" class="block w-full mt-1" type="password" name="password" required autocomplete="current-password" autofocus />
             </div>
 
             <div class="flex justify-end mt-4">
                 <x-jet-button class="ml-4">
-                    {{ __('Confirm') }}
+                    {{ __('ยืนยัน') }}
                 </x-jet-button>
             </div>
         </form>
